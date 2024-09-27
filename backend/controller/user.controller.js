f480bb3c4bd8707f421e0087475eb5cd48713dc9
ali-emumba <ali.usman@emumba.com>
2024-09-27 11:09:15 +0500

    adds cors headersin app

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -130,7 +130,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   console.log(
     "LOGOUT USER",
-    " ------------------------------------------------"
+    " -------------------------------------------------"
   );
 
   const userId = req.user._id;
